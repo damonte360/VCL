7a861f5ddd2d1cb77bc70cc866203aee971cb32c
damonte360 <leica@i.ua>
2025-08-28 18:56:15 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,563 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-villa-chantal",
+      "name": "Villa CHANTAL",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.27262809909868224,
+        "pitch": -0.07636324232566061,
+        "fov": 1.3689172777207057
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.39320805172448203,
+          "pitch": -0.10011318189501317,
+          "rotation": 5.497787143782138,
+          "target": "1-terasa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.34441215618816656,
+          "pitch": -0.4043335342585337,
+          "title": "<strong data-start=\"0\" data-end=\"17\" data-is-only-node=\"\" style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">Villa Chantal</strong>",
+          "text": "<strong data-start=\"0\" data-end=\"17\" data-is-only-node=\"\">Villa Chantal</strong> in Kostanjica, Kotor, Montenegro — a stylish villa with a swimming pool and breathtaking panoramic sea views. The perfect place for a relaxing seaside getaway."
+        },
+        {
+          "yaw": -1.4156179349804034,
+          "pitch": 0.3122431697378545,
+          "title": "<span style=\"font-size: 13px; background-color: rgba(58, 68, 84, 0.8);\">Pool&nbsp;</span>",
+          "text": "Heated with seawater, the villa’s private pool offers a unique experience of swimming with the freshness of the sea and the comfort of warm water. Surrounded by a spacious terrace and breathtaking sea views, it’s the perfect place to relax and unwind."
+        },
+        {
+          "yaw": 2.060032570500546,
+          "pitch": 0.1455210354798684,
+          "title": "Sea",
+          "text": "<article class=\"text-token-text-primary w-full focus:outline-none scroll-mt-[calc(var(--header-height)+min(200px,max(70px,20svh)))]\" tabindex=\"-1\" dir=\"auto\" data-turn-id=\"request-WEB:230c90b7-18f0-4e76-be05-62735c8869c8-8\" data-testid=\"conversation-turn-18\" data-scroll-anchor=\"true\" data-turn=\"assistant\"><div class=\"text-base my-auto mx-auto pb-10 [--thread-content-margin:--spacing(4)] @[37rem]:[--thread-content-margin:--spacing(6)] @[72rem]:[--thread-content-margin:--spacing(16)] px-(--thread-content-margin)\"><div class=\"[--thread-content-max-width:32rem] @[34rem]:[--thread-content-max-width:40rem] @[64rem]:[--thread-content-max-width:48rem] mx-auto max-w-(--thread-content-max-width) flex-1 group/turn-messages focus-visible:outline-hidden relative flex w-full min-w-0 flex-col agent-turn\" tabindex=\"-1\"><div class=\"flex max-w-full flex-col grow\"><div data-message-author-role=\"assistant\" data-message-id=\"fff2b766-a893-4ff8-81f6-974b8f06d853\" dir=\"auto\" class=\"min-h-8 text-message relative flex w-full flex-col items-end gap-2 text-start break-words whitespace-normal [.text-message+&amp;]:mt-5\" data-message-model-slug=\"gpt-5-mini\"><div class=\"flex w-full flex-col gap-1 empty:hidden first:pt-[3px]\"><div class=\"markdown prose dark:prose-invert w-full break-words dark markdown-new-styling\"><p data-start=\"0\" data-end=\"166\" data-is-last-node=\"\" data-is-only-node=\"\">Just 20 steps from the villa, you can reach the stunning Adriatic Sea and the breathtaking Bay of Kotor — perfect for swimming, sunbathing, and enjoying scenic views.</p></div></div></div></div><div class=\"flex min-h-[46px] justify-start\"></div><div class=\"mt-3 w-full empty:hidden\"><div class=\"text-center\"></div></div></div></div></article><div aria-hidden=\"true\" data-edge=\"true\" class=\"pointer-events-none h-px w-px\"></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-terasa",
+      "name": "Terasa",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.330725111566432,
+        "pitch": 0.08012528174366729,
+        "fov": 1.3689172777207057
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.337457407079457,
+          "pitch": 0.17091468736619397,
+          "rotation": 0,
+          "target": "2-room01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-room01",
+      "name": "Room01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.9214126556458915,
+        "pitch": 0.07636137195451553,
+        "fov": 1.3689172777207057
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5327418375843376,
+          "pitch": 0.1936427939807981,
+          "rotation": 5.497787143782138,
+          "target": "8-kitchen"
+        },
+        {
+          "yaw": 2.8378110512911565,
+          "pitch": 0.28967883737562516,
+          "rotation": 0.7853981633974483,
+          "target": "9-pass01"
+        },
+        {
+          "yaw": 1.7863319439558314,
+          "pitch": 0.3635381439610743,
+          "rotation": 0,
+          "target": "10-pass02"
+        },
+        {
+          "yaw": 0.06675494622729694,
+          "pitch": 0.14073014546592688,
+          "rotation": 0.7853981633974483,
+          "target": "1-terasa"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.7800901302433836,
+          "pitch": 0.17239259642621363,
+          "title": "360 tours",
+          "text": "You can book the same 360° tour for yourself. Our website is DAMONTE.net"
+        }
+      ]
+    },
+    {
+      "id": "3-room02",
+      "name": "Room02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1265655079778707,
+          "pitch": 0.2791559496635436,
+          "rotation": 0,
+          "target": "9-pass01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-room03",
+      "name": "Room03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0775774726073166,
+          "pitch": 0.23832253941009185,
+          "rotation": 0,
+          "target": "10-pass02"
+        },
+        {
+          "yaw": -0.12728614144330308,
+          "pitch": 0.09928349128928282,
+          "rotation": 10.995574287564278,
+          "target": "1-terasa"
+        },
+        {
+          "yaw": 0.12274116864992024,
+          "pitch": 0.099373219900313,
+          "rotation": 1.5707963267948966,
+          "target": "0-villa-chantal"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-room04",
+      "name": "Room04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1618634602852822,
+          "pitch": 0.04010126155575122,
+          "rotation": 1.5707963267948966,
+          "target": "0-villa-chantal"
+        },
+        {
+          "yaw": 2.9948385334642182,
+          "pitch": 0.20837138353078544,
+          "rotation": 0,
+          "target": "7-vc02"
+        },
+        {
+          "yaw": -2.5130913441411664,
+          "pitch": 0.24318944299943368,
+          "rotation": 0.7853981633974483,
+          "target": "10-pass02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-vc01",
+      "name": "Vc01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.09800277117719602,
+        "pitch": 0.23665760636497346,
+        "fov": 1.3689172777207057
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3894488392680557,
+          "pitch": 0.23267547518546472,
+          "rotation": 0,
+          "target": "9-pass01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-vc02",
+      "name": "Vc02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.6965257128368236,
+        "pitch": 0.1564476888823645,
+        "fov": 1.3689172777207057
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9203163994058468,
+          "pitch": 0.3241606172964602,
+          "rotation": 0,
+          "target": "5-room04"
+        },
+        {
+          "yaw": 2.3203999103788675,
+          "pitch": 0.3716418495678848,
+          "rotation": 5.497787143782138,
+          "target": "10-pass02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-kitchen",
+      "name": "Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3386804350478627,
+          "pitch": 0.4807494433589632,
+          "rotation": 0,
+          "target": "9-pass01"
+        },
+        {
+          "yaw": -2.5075371759361165,
+          "pitch": 0.4312073822223752,
+          "rotation": 0,
+          "target": "2-room01"
+        },
+        {
+          "yaw": -2.2870646966924486,
+          "pitch": 0.07700953937336585,
+          "rotation": 0.7853981633974483,
+          "target": "1-terasa"
+        },
+        {
+          "yaw": -1.5015967860319677,
+          "pitch": 0.08765255020458618,
+          "rotation": 1.5707963267948966,
+          "target": "10-pass02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pass01",
+      "name": "Pass01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0427642167223077,
+          "pitch": 0.4905435144778494,
+          "rotation": 0.7853981633974483,
+          "target": "8-kitchen"
+        },
+        {
+          "yaw": 2.8196200168477787,
+          "pitch": 0.26638695300611204,
+          "rotation": 5.497787143782138,
+          "target": "2-room01"
+        },
+        {
+          "yaw": 1.7450449449588339,
+          "pitch": 0.3275119691846413,
+          "rotation": 5.497787143782138,
+          "target": "3-room02"
+        },
+        {
+          "yaw": -3.065675943386516,
+          "pitch": 0.07220995517530326,
+          "rotation": 1.5707963267948966,
+          "target": "10-pass02"
+        },
+        {
+          "yaw": -1.048297727338218,
+          "pitch": 0.28052384584292867,
+          "rotation": 5.497787143782138,
+          "target": "6-vc01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-pass02",
+      "name": "Pass02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10530642650811473,
+          "pitch": 0.14471396053339092,
+          "rotation": 0,
+          "target": "5-room04"
+        },
+        {
+          "yaw": -1.602991808413762,
+          "pitch": 0.23080637573413654,
+          "rotation": 0,
+          "target": "4-room03"
+        },
+        {
+          "yaw": 3.1033593525840883,
+          "pitch": 0.021028197872526633,
+          "rotation": 0,
+          "target": "2-room01"
+        },
+        {
+          "yaw": 2.879150817586156,
+          "pitch": 0.2921103058025949,
+          "rotation": 10.995574287564278,
+          "target": "8-kitchen"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Villa CHANTAL",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
